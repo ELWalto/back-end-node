3a82d05798c141282f9a2acd82c1009a1b3093be
ElWalto <walterclean@gmail.com>
2021-09-06 20:34:13 -0300

    adicionando variaveis de ambiente

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 
 const app = express();
 app.use(express.json());
 
+const Conn = require("./models/conn/conn");
 
-const Conn = require("./models/conn/conn")
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
 
-Conn("localhost",27017,"musicas");
+console.log(db_url, db_user, db_pass, db_data);
+Conn(db_url, db_user, db_pass, db_data);
 
 const port = 3000;
 
@@ -18,6 +27,6 @@ const filme = require("./routers/filmes.routes");
 
 app.use("/filmes", filme);
 
-app.listen(process.env.PORT || port,()=> {
-    console.info(`Servidor rodando na porta ${port}`)
+app.listen(process.env.PORT || port, () => {
+  console.info(`Servidor rodando na porta ${port}`);
 });
--- models/conn/conn.js
+++ models/conn/conn.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose");
 // url de conexão mongodb://sevidor:porta/nomeBanco
 
-function Conn(url,porta,banco){
-    mongoose.connect(`mongodb+srv://dbadministrador:8Lw8J1PIfM1rN5Y1@blue-db.upw3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
-        // conecta no bd com as configurações setadas
-        useNewUrlParser: true,
-        useUnifiedTopology: true, // mecanismo de monitoramento de dados
-    }).then(() =>{
-        console.log('Banco connected: ');
-    }).catch((err) =>{
-        console.error(err);
+function Conn(url, user, pass, banco) {
+  mongoose
+    .connect(`${url}/${banco}`, {
+      // conecta no bd com as configurações setadas
+      user: "",
+      pass: "",
+      useNewUrlParser: true,
+      useUnifiedTopology: true, // mecanismo de monitoramento de dados
     })
-};
-
+    .then(() => {
+      console.log("Banco connected: ");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
 
 module.exports = Conn;
